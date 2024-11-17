aafa02ef0b93e52e459428620b23ff77d530804e
4s4r <sesetimokhin@edu.hse.ru>
2024-11-17 22:41:33 +0300

    fixed problem with repeated elements

--- quick_sort.py
+++ quick_sort.py
@@ -6,7 +6,8 @@ def quick_sort(array):
     p = array[0]
     lesser = [array[x] for x in range(cur_len) if array[x] < p]
     greater = [array[x] for x in range(cur_len) if array[x] > p]
-    return quick_sort(lesser) + [p] + quick_sort(greater)
-array = [5, 1, 6, 6, 42, 7, 8]
+    coefficient = 1 if len(lesser) + len(greater) == cur_len else cur_len - (len(lesser) + len(greater))
+    return quick_sort(lesser) + [p] * coefficient + quick_sort(greater)
+array = [5, 1, 6, 6, 42, 7, 8, 8, 8]
 print(quick_sort(array))
 
